8ee39c65bae2921189fd70216b76dac199b3f606
whals <whalsdud0131@gmail.cm>
2025-06-13 11:03:28 +0900

    11:03

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
+class Person {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        if (age > 18 ){
+            this.age = age;
+        } else {
+            System.out.println("성인만 가입 가능.");
+        }
+    }
+    public int getAge() {
+        return age;
+    }
+}
 public class Main {
     public static void main(String[] args) {
+        Person person = new Person();
+
+        person.setName("hello");
+
+        System.out.println(person.getName());
     }
 }
